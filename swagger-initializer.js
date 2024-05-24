65b5b9af281001170bb0e0074f4c801820ba657d
burnhamr <skyy@skyy.cc>
2024-05-24 16:39:54 -0600

    Updated with correct paths

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -2,6 +2,7 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  // http://127.0.0.1:5500/json.json
   window.ui = SwaggerUIBundle({
     url: "https://api.skyy.cc/json.json",
     dom_id: '#swagger-ui',
