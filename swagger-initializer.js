29769b44a18503dcde56a85f5e0bea8e9c9620af
burnhamr <skyy@skyy.cc>
2024-09-27 10:21:09 -0600

    Added new v1 utility routes

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,8 +1,8 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   // http://127.0.0.1:5500/json.json
+  // https://api.skyy.cc/json.json
   window.ui = SwaggerUIBundle({
     url: "https://api.skyy.cc/json.json",
     dom_id: '#swagger-ui',
